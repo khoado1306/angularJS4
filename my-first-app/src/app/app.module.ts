3af384a856e018f6b45310db9832c1b4de244a34
Anh Khoa <anhkhoa@home.com>
2017-09-25 20:50:40 +0700

    1st commit

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //selector:'.app-servers',
+  templateUrl: './servers.component.html',
+  //template:'<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['Testserver','Testserver 2'];
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+   }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
